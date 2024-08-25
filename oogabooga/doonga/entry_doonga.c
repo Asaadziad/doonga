1034d1db6ed29671f4eee7c5a67c6c85384d752d
Asaad ziad <108868994+Asaadziad@users.noreply.github.com>
2024-08-25 11:45:59 +0300

    npc movements

--- oogabooga/doonga/entry_doonga.c
+++ oogabooga/doonga/entry_doonga.c
@@ -1,17 +1,22 @@
 typedef enum {
 	ENTITY_player,
 	ENTITY_fire,
+	ENTITY_mage,
+	ENTITY_range,
+	ENTITY_melee
 } EntityType;
 
 typedef enum {
 	SPRITE_player,
 	SPRITE_fire,
+	SPRITE_range,
+	SPRITE_melee,
+	SPRITE_mage,
 	SPRITE_MAX,
 } SpriteType;
 
 typedef struct Sprite {
 	Gfx_Image* sprite;
-	Vector2    size;
 } Sprite;
 
 typedef struct Entity {
@@ -20,34 +25,87 @@ typedef struct Entity {
 	Vector2    pos;
 
 	u64 spriteId;
+	bool isNpc;
 } Entity;
+// :entity
 
-#define MAX_ENTITIES 1024
+typedef struct WorldFrame {
+	int level;
+} WorldFrame;
+
+#define MAX_ENTITIES 4 * 1024
 
 typedef struct World {
 	Entity entities[MAX_ENTITIES];
 	Sprite sprites[SPRITE_MAX];
+	WorldFrame word_frame; 
 } World;
 
-void load_world_entities(Entity* entities){
-	entities[0] = (Entity){.pos = v2(0,0), .type=ENTITY_player, .spriteId=SPRITE_player, .isValid=true};
-	for(int i = 1; i < 11; i++){
-		entities[i] = (Entity){.pos = v2(get_random_float64_in_range(-100, 100),get_random_float64_in_range(-100, 100)), .type=ENTITY_fire, .spriteId=SPRITE_fire, .isValid=true};
-	}	
+
+
+void load_world_entities(Entity* entities, int level){
+	entities[0] = (Entity){.pos = v2(0,0), .type=ENTITY_player, .spriteId=SPRITE_player, .isValid=true, .isNpc=false};
+	int enemies_to_spawn = get_random_int_in_range(0 + 10 * (level - 1), 10 * level);
+	
+	for(int i = 1; i < enemies_to_spawn; i++) {
+		float64 random_float = get_random_float64_in_range(0, i);
+		int dice = (int)random_float % 3;	
+		float64 random_pos_range_x = get_random_float64_in_range(-200,200);
+		float64 random_pos_range_y = get_random_float64_in_range(-200,200);
+		switch(dice){
+			case 0:
+			entities[i] = (Entity){.pos= v2(random_pos_range_x,random_pos_range_y), .type=ENTITY_melee, .spriteId=SPRITE_melee, .isValid=true, .isNpc=true};
+			break;
+			case 1:
+			entities[i] = (Entity){.pos= v2(random_pos_range_x,random_pos_range_y), .type=ENTITY_range, .spriteId=SPRITE_range, .isValid=true,  .isNpc=true};
+			break;
+			case 2:
+			entities[i] = (Entity){.pos= v2(random_pos_range_x,random_pos_range_y), .type=ENTITY_mage, .spriteId=SPRITE_mage, .isValid=true	 ,	 .isNpc=true};
+			break;
+		}
+	} 
+
+	for(int i = enemies_to_spawn; i < 512 - enemies_to_spawn; i++) {
+		float64 random_pos_range_x = get_random_float64_in_range(-200,200);
+		float64 random_pos_range_y = get_random_float64_in_range(-200,200);
+		entities[i] = (Entity){.pos= v2(random_pos_range_x,random_pos_range_y), .type=ENTITY_fire, .spriteId=SPRITE_fire, .isValid=true,  .isNpc=false};
+	}
+}
+
+Vector2 get_sprite_size(Sprite* sprite){
+	return (Vector2){.x=sprite->sprite->width, .y=sprite->sprite->height};
 }
 
 void load_sprite_images(Sprite* sprites){
-
 	Gfx_Image* player = load_image_from_disk(fixed_string("assets/player.png"), get_heap_allocator());
 	Gfx_Image* fire = load_image_from_disk(fixed_string("assets/fire.png"), get_heap_allocator());
+	Gfx_Image* mage = load_image_from_disk(fixed_string("assets/mage.png"), get_heap_allocator());
+	Gfx_Image* range = load_image_from_disk(fixed_string("assets/range.png"), get_heap_allocator());
+	Gfx_Image* melee = load_image_from_disk(fixed_string("assets/melee.png"), get_heap_allocator());
+	
+	sprites[SPRITE_player] = (Sprite) {.sprite=player};
+	sprites[SPRITE_fire]   = (Sprite) {.sprite=fire};
+	sprites[SPRITE_mage] = (Sprite)   {.sprite=mage};
+	sprites[SPRITE_range]   = (Sprite){ .sprite=range};
+	sprites[SPRITE_melee] = (Sprite)  {.sprite=melee};
+}
 
-	sprites[SPRITE_player] = (Sprite){.size=v2(6.0 , 13.0),.sprite=player};
-	sprites[SPRITE_fire]   = (Sprite){.size=v2(13.0, 14.0), .sprite=fire};
+float64 v2_dist(Vector2 v, Vector2 u){
+	return sqrtf(powf(v.x - u.x, 2) + powf(v.y - u.y, 2));
+}
+
+void move_npc_towards_player(Entity* npc, Vector2 player_pos, float64 delta_t){
+	float64 move_x = player_pos.x - npc->pos.x;
+	float64 move_y = player_pos.y - npc->pos.y;
+	Vector2 input  = (Vector2){.x=move_x, .y=move_y};  
+	input = v2_normalize(input);
+	npc->pos = v2_add(npc->pos, v2_mulf(input, 10 * delta_t));
 }
 
 int entry(int argc, char **argv) {
 	
 	World world = {0};
+	world.word_frame.level = 1;
 
 	window.title = STR("Doonga");
 	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
@@ -57,7 +115,7 @@ int entry(int argc, char **argv) {
 	window.clear_color = hex_to_rgba(0x6495EDff);
 	
 	load_sprite_images(world.sprites);	
-	load_world_entities(world.entities);
+	load_world_entities(world.entities, world.word_frame.level);
 	
 	Entity* player_en = &world.entities[0];
 	float64 last_time = os_get_elapsed_seconds();
@@ -97,8 +155,12 @@ int entry(int argc, char **argv) {
 		
 		for(int i = 0;i < MAX_ENTITIES;i++){
 			if(world.entities[i].isValid){
+				if(world.entities[i].isNpc){
+					move_npc_towards_player(&world.entities[i], player_en->pos, delta);
+				}
 				Sprite entity_sprite = world.sprites[world.entities[i].spriteId];
-				draw_image(entity_sprite.sprite, v2(world.entities[i].pos.x - 0.5 * entity_sprite.size.x, world.entities[i].pos.y), entity_sprite.size, COLOR_WHITE);
+				Vector2 sprite_size = get_sprite_size(&entity_sprite);
+				draw_image(entity_sprite.sprite, v2(world.entities[i].pos.x - 0.5 * sprite_size.x, world.entities[i].pos.y), get_sprite_size(&entity_sprite), COLOR_WHITE);
 			}
 		}
 		
